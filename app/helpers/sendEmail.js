af179514034598526ea3e3737347103566c9915a
chaerulmarwan20 <chaerulmarwan20@gmail.com>
2021-05-24 11:45:58 +0700

    fix sendEmail

--- app/helpers/sendEmail.js
+++ app/helpers/sendEmail.js
@@ -1,8 +1,6 @@
 const nodemailer = require("nodemailer");
 const smtpTransport = require("nodemailer-smtp-transport");
-const host = process.env.HOST;
-const port = process.env.PORT_FRONTEND;
-const link = `http://${host}:${port}`;
+const link = `https://coffee-shop-bohdan.netlify.app`;
 const email = process.env.EMAIL_USER;
 const password = process.env.EMAIL_PASS;
 
@@ -23,16 +21,47 @@ const send = (destination, token, type) => {
         const info = await transporter.sendMail({
           from: email,
           to: destination,
-          subject: "Account Verification",
-          html: `Click this link to verify your account : <a href="${link}/sign-up/?email=${destination}&token=${token}">Activate</a>`,
+          subject: "Email Verification - Coffee Shop",
+          html: `
+            Hello ${destination},
+            <br/>
+            <br/>
+            Thank you for registering on the Coffee Shop app!
+            <br/>
+            <br/>
+            In order for your email to be used for logging in, we need to verify your email address. Please use the link below to confirm your email address and complete the process.
+            <br/>
+            <br/>
+            Click this link to verify your account : <a href="${link}/sign-up/?email=${destination}&token=${token}">Verify Now</a>
+            <br/>
+            <br/>
+            Thank you,
+            <br/>
+            <br/>
+            The Coffee Shop team
+          `,
         });
         resolve(info);
       } else if (type === "forgot") {
         const info = await transporter.sendMail({
           from: email,
           to: destination,
-          subject: "Reset Password",
-          html: `Click this link to reset your password : <a href="${link}/forgot-password/?email=${destination}&token=${token}">Reset Password</a>`,
+          subject: "Reset Password - Coffee Shop",
+          html: `
+            Hello ${destination},
+            <br/>
+            <br/>
+            Please use the link below to reset your password and complete the process.
+            <br/>
+            <br/>
+            Click this link to reset your password : <a href="${link}/forgot-password/?email=${destination}&token=${token}">Reset Now</a>
+            <br/>
+            <br/>
+            Thank you,
+            <br/>
+            <br/>
+            The Coffee Shop team
+          `,
         });
         resolve(info);
       }
